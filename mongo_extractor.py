ac4de48f42175868f415c43081b6ba49e60b125c
Yuvan Chugh <yuvanchugh7@gmail.com>
2020-10-17 21:31:53 +0530

    Updated gitignore, added mongo documentation

--- mongo_extractor.py
+++ mongo_extractor.py
@@ -11,13 +11,35 @@ class MongoExtractor():
         self.set_url_cursor()
 
     def set_url_connection(self, db_name, collection_name):
+        """
+        Sets connection for [collection_name] in [db_name]
+
+        :param db_name: db name
+        :param collection_name: collection name
+        :return:
+        """
         self.urlCollection = self.client[db_name][collection_name]
 
     def set_url_cursor(self):
+        """
+        Sets cursor for connection
+        :return:
+        """
         self.url_cursor = self.urlCollection.aggregate([{'$unwind': {'path': "$client_info.website_info", 'preserveNullAndEmptyArrays': True}}])
 
-    def insert_record(self, article):
-        db = self.client.output_database
+    def insert_record(self, article, db_url):
+        """
+        Inserts record with [article] contents into MongoDB
+
+        :param article: contents of record
+        :param db_url: db url
+        :return:
+        """
+        client = self.client
+        if db_url is not None:
+            client = MongoClient(db_url)
+
+        db = client.output_database
         db.output_collection.insert_one({'doc': article})
 
 
@@ -26,4 +48,3 @@ if __name__ == '__main__':
     db_name = 'sample_db'
     collection_name = 'sample_collection'
     MongoExtractor(db_url, db_name, collection_name)
-
